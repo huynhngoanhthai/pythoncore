99ea31e0038733bd28ccfe926f1ae655ea65f7b0
huynhngoanhthai <huynhngoanhthai@gmail.com>
2026-04-04 20:30:41 +0700

    add code buổi 2

---
+++ day2/main.py
@@ -0,0 +1,98 @@
+# ten-ham + ()
+print()
+
+# string cung duoc hieu nhu 1 array
+a = 'thai'
+# print(a[-1]) 
+
+a = ['thai', 'thanh', 'tam'] 
+# print(len(a)) 
+# 4
+
+# a = 1203
+# 1.(33)
+# 1
+
+# 1 array 
+# 
+# [start:end:step]
+# 1203
+# [::-1]
+# 1 -> 4
+# 3001
+
+# print(str(a)[::-3]) 
+# 03 01
+
+
+#  ham input
+
+# a = input()
+
+# print("ket qua", a)
+# nhap vao 2 so a b sao do nhap 1 trong (+-*/)
+# in ra ket qua phep tinh 
+
+# buoi lam gi
+# a = int(input())
+# b = int(input())
+# phepTinh = input()
+
+
+
+# if phepTinh == '+':
+#     print(a + b)
+
+# if phepTinh == '-':
+#     print(a - b)
+
+# if phepTinh == '*':
+#     print(a * b)
+
+# if phepTinh == '/':
+#     print(a / b)
+
+
+# a = str(1233)
+
+# a = [5,2,3,4,1]
+# a.sort()
+# min max
+# print(a)
+
+# ten bien = gia tri
+#  minh ko dc phet dat so truong 
+
+# nhap form
+# ten,=input('Tên: ')
+# tuoi =input('tuổi: ')
+# # in form
+# print('=============FORM=============' )
+# print('Tên:',ten )
+# print('Độ dài tên:', len(ten)) # tu tinh ra
+# print('Tuổi:', tuoi)
+# print('Năm sinh:', (2026 - int(tuoi))) # tư tinh
+
+
+# khai bao nhiu bien tren 1 dong
+# a = 2,b = 1
+# a,b = 1, 2
+# print(a,b)
+
+# nhap vao so tiền n và số người k 
+
+# chia tiền cho mn 
+# neu n > 10.000 thì phai chịu thêm 10% thuê
+# in ra kết qua 1 người cần tra bao nhiu tien
+# day là vnd không sô số lẻ 
+
+# 100.000 5
+# [1,2,5,1,2] = 11
+
+# neu n > 10.000 thì phai chịu thêm 10% thuê
+# người 1 phải trả = 1/11 * tông tiền =
+# người 2 phải trả = 2/11 * tông tiền = 
+# người 3 phải trả = 5/11 * tông tiền = 
+# người 4 phải trả = 1/11 * tông tiền = 
+# người 5 phải trả = 2/11 * tông tiền = 
+
