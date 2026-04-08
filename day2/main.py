8ec7dcc392f13faaafd9e87d6ca7212f801bff03
huynhngoanhthai <huynhngoanhthai@gmail.com>
2026-04-08 20:25:49 +0700

    add code main

--- day2/main.py
+++ day2/main.py
@@ -1,104 +1,107 @@
-# ten-ham + ()
-print()
+import math
 
-# string cung duoc hieu nhu 1 array
-a = 'thai'
-# print(a[-1]) 
+# a = 12
+# b = "so"
+# print("Hello Python", a,b )
 
-a = ['thai', 'thanh', 'tam'] 
-# print(len(a)) 
-# 4
+# ham len()
 
-# a = 1203
-# 1.(33)
-# 1
+# a = "thai"
+# lista = [1,2,3,4,5]
+# print(len(lista))
 
-# 1 array 
-# 
-# [start:end:step]
-# 1203
-# [::-1]
-# 1 -> 4
-# 3001
+# type
 
-# print(str(a)[::-3]) 
-# 03 01
+# a = 12
+# print(type(a))
 
+# a = """
+# a
 
-#  ham input
+# a
+# """
 
-# a = input()
+# print(type(a))
 
-# print("ket qua", a)
-# nhap vao 2 so a b sao do nhap 1 trong (+-*/)
-# in ra ket qua phep tinh 
+# int() ép kiêu dữ liêu thành kieu du lieu int
+# no so nguyen 3.14 => 3 
 
-# buoi lam gi
-# a = int(input())
-# b = int(input())
-# phepTinh = input()
+# a = 3.14
+# print(type(a))
+# a = int('300')
+# # "300" + "3" = "3003"
+# print("300" + "3")
+# print(type(a), a + 3)
 
-
-
-# if phepTinh == '+':
-#     print(a + b)
-
-# if phepTinh == '-':
-#     print(a - b)
-
-# if phepTinh == '*':
-#     print(a * b)
-
-# if phepTinh == '/':
-#     print(a / b)
-
-
-# a = str(1233)
-
-# a = [5,2,3,4,1]
-# a.sort()
-# min max
+# 12/12/2012 => unit-time
+# float()
+# a = float('300.1')
 # print(a)
 
-# ten bien = gia tri
-#  minh ko dc phet dat so truong 
+# từ số thanh chữ str() 
+# a = 999003
+# k = str(a)
 
-# nhap form
-# ten,=input('Tên: ')
-# tuoi =input('tuổi: ')
-# # in form
-# print('=============FORM=============' )
-# print('Tên:',ten )
-# print('Độ dài tên:', len(ten)) # tu tinh ra
-# print('Tuổi:', tuoi)
-# print('Năm sinh:', (2026 - int(tuoi))) # tư tinh
+# print(int(k[-1]) + 4)
+
+#  input() đầu vào hay còn nhạp từ bàn phím
+# nhap ten va nam sinh tin ra in ra tuoi va ten
+# ten = input("ten: ")
+# nam = int(input("nam sinh: "))
 
 
-# khai bao nhiu bien tren 1 dong
-# a = 2,b = 1
-# a,b = 1, 2
-# print(a,b)
+# print("ten của ban la: ", ten)
+# print("tuoi: ", 2026 - nam)
 
-# nhap vao so tiền n và số người k 
+# dict()
 
-# chia tiền cho mn 
-# neu n > 10.000 thì phai chịu thêm 10% thuê
-# in ra kết qua 1 người cần tra bao nhiu tien
-# day là vnd không sô số lẻ 
+# list()
+# a = [1,2,23]
+# print(list(a))
 
-# 100.000 5
-# [1,2,5,1,2] = 11
+# ham dat biet 
+# ds = [3,5,6,12,3,6,21,22,90]
 
-# neu n > 10.000 thì phai chịu thêm 10% thuê
-# người 1 phải trả = 1/11 * tông tiền =
-# người 2 phải trả = 2/11 * tông tiền = 
-# người 3 phải trả = 5/11 * tông tiền = 
-# người 4 phải trả = 1/11 * tông tiền = 
-# người 5 phải trả = 2/11 * tông tiền = 
+#  max , min, sorted, sum
+# print(sorted(ds, reverse=True))
 
-### gợi ý dùng for dể in ra
-# lista = [1,2,5,1,2]
+# bien
+# khong dc bat dau bang so
+# 2thai =1
+# thai-huynh
+# ho#ten 
+# thai_huynh = 
+# _ ABC abc 2
+# tranh dat ten trung boi chữ đặt biệt 
+# b2c = 123
 
-# for a in lista:
-#     print(a)
-# lista.
+# 1 dong co nhiu biên
+# ten, tuoi, nam =  "thai", 26, 2000
+# hoTen
+# ho_ten
+# print(ten, tuoi, nam )
+
+# S = 1 + 3 + 5 +7 + ... + n
+# n = 5
+# S = 1 + 3 + 5 + 7 + 9 = 25
+
+# 5 -> 25
+# 4 -> 16
+# 3 -> 9
+# 2 -> 4
+
+# r = int(input())
+#
+# a = math.gcd(9, 6)
+# 0.3333333
+# 33.3333
+# 33
+# 0.33
+
+# a = int(a *100) / 100
+# r = input("")
+
+# 33 / 4 = 8.25
+print(int(33 / 4))
+print(33 // 4)
+
